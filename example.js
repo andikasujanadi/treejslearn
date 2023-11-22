5e5633763ee0343cc0bc55ae22b483c1f8038cae
Andika Sujanadi <51147116+andikasujanadi@users.noreply.github.com>
2023-11-23 00:59:28 +0800

    remove lenis css

--- example.js
+++ example.js
@@ -3,8 +3,8 @@ const scene = new THREE.Scene();
 
 // create camera
 const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 20;
-camera.position.y = 10;
+camera.position.z = 10;
+camera.position.y = 5;
 camera.rotation.x = -0.5;
 
 const moveCamera = (x) => {
