bb404356940c289600f965e4837a206ab4781d16
Andika Sujanadi <51147116+andikasujanadi@users.noreply.github.com>
2023-11-26 22:48:21 +0800

    add gsap still bug

--- example.js
+++ example.js
@@ -1,21 +1,82 @@
+// template data
+const options = {
+    dirLight: '#FFFFFF',
+    ambLight: '#A5A5A5',
+}
+
+const cameraOption = {
+    cameraX: 0.1,
+    cameraY: 8.0,
+    cameraZ: 4.5,
+    cameraLookX:0.1,
+    cameraLookY:-1.0,
+    CameraLookZ:0.1,
+}
+
+const cameraCut1 = {
+    cameraX: 0,
+    cameraY: 8,
+    cameraZ: 4.5,
+    cameraRotationX:-1.1071487177940904,
+    cameraRotationY:0,
+    CameraRotationZ:0,
+}
+
+const cameraCut2 = {
+    cameraX: 0,
+    cameraY: 2.5,
+    cameraZ: 4.5,
+    cameraRotationX:-0.32175055439664224,
+    cameraRotationY:0,
+    CameraRotationZ:0,
+}
+
+const cameraCut3 = {
+    cameraX: 2.3,
+    cameraY: 2.5,
+    cameraZ: 3.8,
+    cameraRotationX:-1.1136189251769462,
+    cameraRotationY:0.49956010330636486,
+    CameraRotationZ:0.7721146406652785,
+}
+
+const cameraCut4 = {
+    cameraX: 3.9,
+    cameraY: 3.0,
+    cameraZ: 1.8,
+    cameraRotationX:-1.2610670403004278,
+    cameraRotationY:0.5507373140124942,
+    CameraRotationZ:1.0219339106944585,
+}
+
+const cameraCut5 = {
+    cameraX: 4.5,
+    cameraY: 3.1,
+    cameraZ: -3.7,
+    cameraRotationX:-2.3075552143175866,
+    cameraRotationY:0.4822415270203947,
+    CameraRotationZ:2.669004141505082,
+}
+
+const cameraCut6 = {
+    cameraX: -2,
+    cameraY: 3.6,
+    cameraZ: -4.5,
+    cameraRotationX:-1.9723641818256934,
+    cameraRotationY:0.45683107579898935,
+    CameraRotationZ:-2.3371799539275013,
+}
+
 // create scene
 const scene = new THREE.Scene();
+scene.background = new THREE.Color('#e5e5e5');
 
 // create camera
 const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 2;
-camera.position.y = 3;
-camera.position.x = 0;
-camera.lookAt(0,.5,0);
-
-const moveCamera = (x) => {
-    const resolution = 100;
-    for (let index = 0; index < Math.abs(x)*resolution; index++) {
-        setTimeout(() => {
-            camera.position.x += Math.abs(x)/x/resolution;
-        }, index);
-    }
-}
+camera.position.x = cameraOption.cameraX
+camera.position.y = cameraOption.cameraY
+camera.position.z = cameraOption.cameraZ
+camera.lookAt(cameraOption.cameraLookX,cameraOption.cameraLookY,cameraOption.CameraLookZ);
 
 // render scene
 const renderer = new THREE.WebGLRenderer({antialias:true});
@@ -25,62 +86,25 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 threeContainer.appendChild(renderer.domElement);
 
 // create camera orbit
-// const orbit = new THREE.OrbitControls(camera, renderer.domElement)
-// orbit.update;
+const orbit = new THREE.OrbitControls(camera, renderer.domElement)
+orbit.update;
 
 // add light
-const ambientLight = new THREE.AmbientLight(0x505090);
+const ambientLight = new THREE.AmbientLight(0xA5A5A5);
 scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight(0xFFFF70);
-directionalLight.position.set(-20,10,20);
+const directionalLight = new THREE.DirectionalLight(0xFFFFFF);
+directionalLight.position.set(3,10,3);
 directionalLight.castShadow = true;
 directionalLight.shadow.bias = -0.0002;
 scene.add(directionalLight);
 
-const dLightHelper = new THREE.DirectionalLightHelper(directionalLight);
-scene.add(dLightHelper);
-
-const dLightShadowHelper = new THREE.DirectionalLightHelper(directionalLight);
-scene.add(dLightShadowHelper);
-
-// create 3d object
-const axesHelper = new THREE.AxesHelper(20);
-const gridHelper = new THREE.GridHelper(30);
-
-const cylinderGeometry = new THREE.CylinderBufferGeometry(1, 1, 2, 12);
-const CylinderMaterial = new THREE.MeshStandardMaterial({
-    color: '#8bc34a',
-    // wireframe: true,
-});
-const cylinder = new THREE.Mesh(cylinderGeometry, CylinderMaterial);
-cylinder.position.x = -2;
-cylinder.position.y = 1;
-cylinder.castShadow = true;
-cylinder.receiveShadow = true;
-
-const planeGeometry = new THREE.PlaneGeometry(30,30);
-const planeMaterial = new THREE.MeshStandardMaterial({
-    color: '#FFFFFF',
-    side: THREE.DoubleSide
-
-});
-const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-plane.rotation.x = - 0.5 * Math.PI;
-plane.receiveShadow = true;
-
-// load suzanne without default material
+// load model
 const loader = new THREE.GLTFLoader();
-var suzzane_material = new THREE.MeshStandardMaterial({
-    // color: '#FF0000',
-    // side: THREE.DoubleSide
-
-});
+var suzzane_material = new THREE.MeshStandardMaterial();
 var suzanne;
 loader.load( 'ring.glb', function (gltf) {
     suzanne = gltf.scene
-    suzanne.position.y = 0.3;
-    suzanne.position.x = 0.3;
     suzanne.traverse(function(child){
         if (child instanceof THREE.Mesh) {
             // child.material = suzzane_material;
@@ -93,36 +117,103 @@ loader.load( 'ring.glb', function (gltf) {
 	console.error( error );
 });
 
-
-// add object to scene
-// scene.add(axesHelper);
-scene.add(gridHelper);
-scene.add(cylinder);
-scene.add(plane);
-scene.background = new THREE.Color('#e5e5e5');
-
 // animate object
 const animate = () => {
     requestAnimationFrame(animate);
 
-    cylinder.rotation.x += 0.01;
-    cylinder.rotation.z += 0.01;
-
     renderer.render(scene, camera);
 }
 
-// call animate to display
-animate();
+// add dat gui
+const centerCamera = () =>{
+    camera.lookAt(cameraOption.cameraLookX,cameraOption.cameraLookY,cameraOption.CameraLookZ);
+}
+const gui = new dat.GUI();
+gui.addColor(options, 'dirLight').onChange(function(e){
+    directionalLight.color.set(e);
+    centerCamera();
+});
+gui.addColor(options, 'ambLight').onChange(function(e){
+    ambientLight.color.set(e);
+    centerCamera();
+});
+gui.add(cameraOption, 'cameraX').onChange(function(e){
+    camera.position.x = e;
+    centerCamera();
+});
+gui.add(cameraOption, 'cameraY').onChange(function(e){
+    camera.position.y = e;
+    centerCamera();
+});
+gui.add(cameraOption, 'cameraZ').onChange(function(e){
+    camera.position.z = e;
+    centerCamera();
+});
+gui.add(cameraOption, 'cameraLookX').onChange(function(e){
+    camera.lookAt.x = e;
+    centerCamera();
+});
+gui.add(cameraOption, 'cameraLookY').onChange(function(e){
+    camera.lookAt.y = e;
+    centerCamera();
+});
+gui.add(cameraOption, 'CameraLookZ').onChange(function(e){
+    camera.lookAt.z = e;
+    centerCamera();
+});
 
 // responsive canvas
-window.addEventListener('resize', function() {
+window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
-})
+});
 
 // scroll
 window.onscroll = function() {
     var scrollPosition = window.scrollY;
     suzanne.rotation.y=-scrollPosition/500;
-  };
+};
+
+// GSAP testing
+window.addEventListener('mousedown', () => {
+    
+});
+
+const cameras = [
+    cameraCut1,
+    cameraCut2,
+    cameraCut3,
+    cameraCut4,
+    cameraCut5,
+    cameraCut6,
+]
+
+index = 0;
+
+const next = () => {
+    const angle = cameras[index];
+    index+=1;
+    if(index>=cameras.length){
+        index = 0
+    }
+    gsap.to(camera.position, {
+        x:angle.cameraX,
+        y:angle.cameraY,
+        z:angle.cameraZ,
+        duration:1.5,
+        onUpdate: () =>{
+            // camera.lookAt(angle.cameraLookX,angle.cameraLookY,angle.CameraLookZ);
+        }
+    });
+
+    gsap.to(camera.rotation, {
+        x:angle.cameraRotationX,
+        y:angle.cameraRotationY,
+        z:angle.CameraRotationZ,
+        duration:1.5,
+    });
+}
+
+// call animate to display
+animate();
